b34e78461bdb1045ee283af028a126f546b9e0a6
Juan Aleman <juan.j.aleman@gmail.com>
2020-12-07 20:24:44 -0600

    code: BigO notation example

---
+++ Python/UPER/time-and-space1.py
@@ -0,0 +1,21 @@
+"""
+Given an array of integers `nums` and an integer `target`, return the indices
+of the two numbers that add up to the `target`.
+Examples:
+- two_sum(nums = [2,5,9,13], target = 7) -> [0,1] (nums[0] + nums[1] == 7)
+- two_sum(nums = [4,3,5], target = 8) -> [1,2] (nums[1] + nums[2] == 8)
+Notes:
+- Each input will have only one solution.
+- You may not use the same element twice.
+- You can return the answer in any order.
+"""
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            num1 = nums[i]
+            num2 = nums[j]
+            if num1 + num2 == target:
+                return  [i, j]
+    return None
+
+print(two_sum(nums = [2,5,9,13], target = 7))
